5b666782c33a1d81e28dcaee953d2adceb3e6890
Nazzik1992 <tkachuknazarii15@gmail.com>
2023-01-05 14:00:23 +0200

    addscriptlibrary

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,33 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+  
+const galeryEl = document.querySelector('.gallery');
+
+
+const galeryImages = galleryItems
+.map(image =>
+    `<div class="gallery__item">
+    <a class="gallery__link" 
+    href="${image.original}">
+    <img class="gallery__image" src="${image.preview}" data-source="${image.original}" alt="${image.description}"/> 
+    </a>
+    </div>`
+    );
+
+    galeryEl.insertAdjacentHTML('afterbegin', galeryImages.join(''));
+    galeryEl.addEventListener("click", onImgClick);
+
+    function onImgClick(event) {
+        event.preventDefault();
+        if (event.target.nodeName !== "IMG") {
+            return;
+        }
+        
+    const instance = basicLightbox.create(`<img src="${event.target.dataset.source}" >`)
+
+    instance.show()
+    }
+
+
+   
